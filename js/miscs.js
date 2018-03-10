056f8c3f807190b9030837f4ae87064ba679dd25
Cristi Hasna <cristian.hasna@gmail.com>
2018-03-10 13:32:46 +0200

    Header & Footer, font-awesome and html base struct

---
+++ js/miscs.js
@@ -0,0 +1,17 @@
+document.getElementById('nav-trigger').addEventListener("click", function(){
+	document.getElementById('nav-menu').classList.toggle('visible');
+	document.getElementById('nav-trigger').classList.toggle('triggered');
+});
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth(); //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd = '0'+dd
+} 
+
+var months = ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Iun', 'Iul', 'Aug', 'Sep', 'Oct', 'Nov',];
+today = dd + '-' +months[mm]+'-' + yyyy;
+document.getElementById('current-date').innerHTML=today;

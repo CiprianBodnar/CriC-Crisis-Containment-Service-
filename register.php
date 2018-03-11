f5250585b31b74ffd5e75e1fc8c1be2fa7f12136
mircearares <mirceararesgabriel@gmail.com>
2018-03-11 21:08:36 +0200

    Update la resetat parola si inregistrare

--- register.php
+++ register.php
@@ -15,68 +15,70 @@
 	
 	<section id="content">
         <div class="gradient-bg">
-                <div class="container orange" id="enlarge">
-                    <div class="row">
-                        <div class="col12">
-                            <h3 class="subtitle">
-                                Inregistrarea contului dvs
-                            </h3>
-                            <form class="contact-form">
-                                <div class="row">
-                                    <div class="col6 no-padding">
-                                        <div class="par">
-                                            Nume familie
-                                        </div>
-                                        <input type="text" name="Nume" value="Nume" onfocus="if(this.value=='Nume') this.value='';" onblur="if(this.value=='') this.value='Nume';">
-                                    </div>
-                                    <div class="col6 no-padding">
+                <div class="container no-padding">
+                    <div class="box-medium align-left orange" id="enlarge">
+                        <div class="row">
+                            <div class="col12">
+                                <h3 class="subtitle">
+                                    Inregistrarea contului dvs
+                                </h3>
+                                <form class="contact-form">
+                                    <div class="row">
+                                        <div class="col6 no-padding">
                                             <div class="par">
-                                                Prenume 
+                                                Nume familie
                                             </div>
-                                            <input type="text" name="Prenume" value="Prenume" onfocus="if(this.value=='Prenume') this.value='';" onblur="if(this.value=='') this.value='Prenume';">
-                                    </div>
-
-                                    <div class="col6 no-padding">
-                                        <div class="par">
-                                            Adresa de e-mail
+                                            <input type="text" name="Nume" value="Nume" onfocus="if(this.value=='Nume') this.value='';" onblur="if(this.value=='') this.value='Nume';">
                                         </div>
-                                        <input type="text" name="Email" value="your.email@yoursite.com" onfocus="if(this.value=='your.email@yoursite.com') this.value='';" onblur="if(this.value=='') this.value='your.email@yoursite.com';">
-                                    </div>
-
-                                    <div class="col6 no-padding">
-                                        <div class="par">
-                                            Verificare adresa de e-mail
+                                        <div class="col6 no-padding">
+                                                <div class="par">
+                                                    Prenume 
+                                                </div>
+                                                <input type="text" name="Prenume" value="Prenume" onfocus="if(this.value=='Prenume') this.value='';" onblur="if(this.value=='') this.value='Prenume';">
                                         </div>
-                                        <input type="text" name="Verificare Email" value="your.email@yoursite.com" onfocus="if(this.value=='your.email@yoursite.com') this.value='';" onblur="if(this.value=='') this.value='your.email@yoursite.com';">
-                                    </div>
 
-
-                                    <div class="col6 no-padding">
-                                        <div class="par">
-                                            Parola
+                                        <div class="col6 no-padding">
+                                            <div class="par">
+                                                Adresa de e-mail
+                                            </div>
+                                            <input type="text" name="Email" value="your.email@yoursite.com" onfocus="if(this.value=='your.email@yoursite.com') this.value='';" onblur="if(this.value=='') this.value='your.email@yoursite.com';">
                                         </div>
-                                        <input type="password" class="pwd" name="Parola" value="Parola" onfocus="if(this.value=='Parola') this.value='';" onblur="if(this.value=='') this.value='Parola';">
-                                    </div>
-                                    <div class="col6 no-padding">
-                                        <div class="par">
-                                            Verificare parola
+
+                                        <div class="col6 no-padding">
+                                            <div class="par">
+                                                Verificare adresa de e-mail
+                                            </div>
+                                            <input type="text" name="Verificare Email" value="your.email@yoursite.com" onfocus="if(this.value=='your.email@yoursite.com') this.value='';" onblur="if(this.value=='') this.value='your.email@yoursite.com';">
                                         </div>
-                                        <input type="password" class="pwd" name="Verificare parola" value="Parola" onfocus="if(this.value=='Verificare parola') this.value='';" onblur="if(this.value=='') this.value='Verificare parola';">
-                                    </div>
-                                    <div class="par">
-                                        Adresa
-                                    </div>
 
-                                    <input type="text" name="Adresa" value="Adresa" onfocus="if(this.value=='Adresa') this.value='';" onblur="if(this.value=='') this.value='Adresa';">
 
-                                    <button type="submit" id="submit-button">
-                                        Trimitere
-                                    </button>
-                                    <div class="clear"></div>
-                                </div>
-                            </form>
+                                        <div class="col6 no-padding">
+                                            <div class="par">
+                                                Parola
+                                            </div>
+                                            <input type="password" class="pwd" name="Parola" value="Parola" onfocus="if(this.value=='Parola') this.value='';" onblur="if(this.value=='') this.value='Parola';">
+                                        </div>
+                                        <div class="col6 no-padding">
+                                            <div class="par">
+                                                Verificare parola
+                                            </div>
+                                            <input type="password" class="pwd" name="Verificare parola" value="Parola" onfocus="if(this.value=='Verificare parola') this.value='';" onblur="if(this.value=='') this.value='Verificare parola';">
+                                        </div>
+                                        <div class="par">
+                                            Adresa
+                                        </div>
+
+                                        <input type="text" name="Adresa" value="Adresa" onfocus="if(this.value=='Adresa') this.value='';" onblur="if(this.value=='') this.value='Adresa';">
+
+                                        <button type="submit" id="submit-button">
+                                            Trimitere
+                                        </button>
+                                        <div class="clear"></div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="clear"></div>
                         </div>
-                        <div class="clear"></div>
                     </div>
                 </div>
             </div>

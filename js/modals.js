463bd557da4bb3eeb4119b6b50c4c7782d42cc29
VartolomeiLucian <lucianvartolomei16@gmail.com>
2018-03-19 00:26:54 +0200

    Update popups-Home Page

--- js/modals.js
+++ js/modals.js
@@ -2,11 +2,30 @@ $('.modal-close').on('click', function(){
 	$('.modal').removeClass('visible');
 	$('.cover').fadeOut(200);
 });
+
 $('#search-trigger').on('click', function(){
 	$("#search").addClass('visible');
 	$('.cover').fadeIn(200);
 });
+
+$('#share-trigger').on('click', function(){
+	$("#share").addClass('visible');
+	$('.cover').fadeIn(200);
+});
+
+
 $('.cover').on("click", function(){
 	$('.modal').removeClass('visible');
 	$(this).fadeOut(200);
 })
+
+ $(function(){
+    $('input[name="showthis"]').hide();
+    $('input[name="checkbox"]').on('click', function(){
+        if ($(this).prop('checked')){
+            $('input[name="showthis"]').fadeIn();
+        } else{
+            $('input[name="showthis"]').hide();
+        }
+    });
+});
